fe05ded730b01a53706ed8481405e8ac4a8644e8
TsvetelinIliev <bho981@gmail.com>
2026-09-04 18:55:41 +0100

    Fix env issue

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_API_URL,
+    baseURL: process.env.EXPO_PUBLIC_API_URL, 
     timeout: 10000,
 
 })
